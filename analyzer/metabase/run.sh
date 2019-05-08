51caadb1d3350430483a42cb9f78901b3ad59291
Felipe Nunes Gaia <felipegaia.comp@gmail.com>
2019-05-08 13:24:56 -0300

    Save data of config form.
    Changes made to save the configurations in the quality model.

---
+++ analyzer/metabase/run.sh
@@ -0,0 +1,12 @@
+!#/bin/bash
+
+# export DB_USERNAME=???
+# more vars here as needed...
+# user
+# pass
+# ... 
+
+# restore metabase backup
+
+# Call the original entrypoint
+/app/run_metabase.sh $@
--- analyzer/src/main/java/eubrazil/atmosphere/controller/DashboardController.java
+++ analyzer/src/main/java/eubrazil/atmosphere/controller/DashboardController.java
@@ -1,23 +1,32 @@
 package eubrazil.atmosphere.controller;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import eubrazil.atmosphere.entity.dashboard.Properties;
-import eubrazil.atmosphere.entity.dashboard.PropertiesJsonMap;
-import eubrazil.atmosphere.service.impl.DashboardService;
+import eubrazil.atmosphere.qualitymodel.Attribute;
+import eubrazil.atmosphere.repository.AttributeRepository;
+import eubrazil.atmosphere.service.impl.AttributeService;
+import eubrazil.atmosphere.qualitymodel.Preference;
+import eubrazil.atmosphere.repository.PreferenceRepository;
+import eubrazil.atmosphere.service.impl.PreferenceService;
+import eubrazil.atmosphere.config.appconfig.PropertiesManager;
 
 /**
-* Dashboard Controller
-* @author JorgeLuiz
-* @version 1.0
+ * Dashboard Controller
+ * @author JorgeLuiz
+ * @version 1.0
+ * @author Felipe Gaia
  */
 @RestController
 @RequestMapping("/dash")
@@ -27,19 +36,68 @@ public class DashboardController {
 public static final Logger LOGGER = LoggerFactory.getLogger(DashboardController.class); 
 	
 	@Autowired
-	private DashboardService dashBoardService;
+	private AttributeService attributeService;
+
+	@Autowired
+	private PreferenceService preferenceService;
 	
+	@CrossOrigin
 	@RequestMapping(value = "/save", method = RequestMethod.POST)
 	@ResponseBody
-	public void save(@RequestBody PropertiesJsonMap prop) {
-		Properties newProp = new Properties();
-		newProp.setName(prop.getPropRelevance()[0].getName());
-		newProp.setRelevance(prop.getPropRelevance()[0].getData());
-		newProp.setThreshold(prop.getThresholds()[0].getData());
-		newProp.setPeriodicity(prop.getPeriodicity()[0].getData());
-		dashBoardService.save(newProp);
-		newProp.toString();
-	}
+	public void save(@RequestBody String input) {
 	
-	// GET properties
+		// load json values into object	
+		JSONObject json = new JSONObject(input);
+		JSONArray properties = json.getJSONArray("json");
+		
+		JSONObject item;
+		Preference p;
+		
+		// for each property (metric)
+		for(int i=0; i < properties.length(); i++) {
+    		// recover json object
+    		item = properties.getJSONObject(i);
+            
+            // update value into db
+            p = preferenceService.getPreferenceById(item.getInt("Id"));
+            p.setThreshold(item.getDouble("Threshold"));
+            p.setWeight(item.getDouble("Relevance"));
+            preferenceService.save(p);
+        }
+ 	}
+	
+	@CrossOrigin
+	@RequestMapping(value = "/get", method = RequestMethod.GET, produces = "application/json")
+	@ResponseBody
+	public String get() {
+
+		JSONArray properties = new JSONArray();
+		JSONObject item;
+		Preference p;
+		Integer jobTime;
+
+		// for each property (also called attribute or metric)
+		for(Attribute a : attributeService.getAllAttributes()) {
+			// create a json object and include metric (id, name)
+			item = new JSONObject();
+			item.put("Id", a.getAttributeId());
+			item.put("Name", a.getName());
+			
+			// recover other values by Preference db table
+			p = preferenceService.getPreferenceById(a.getAttributeId());
+			
+			// recover job time by config.properties file
+			jobTime = Integer.parseInt(PropertiesManager.getInstance().getProperty("trigger.job.time").split("/")[1]);
+
+			// include other values
+			item.put("Relevance", p.getWeight());
+			item.put("Threshold", p.getThreshold());
+			item.put("Periodicity", jobTime);
+			
+			// add json object into array
+			properties.put(item);
+		}
+
+		return properties.toString();
+	}
 }
